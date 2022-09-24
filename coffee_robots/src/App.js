d97863643a786d062ed90e24b706c389bea81ebf
Rhys Swansbra <r.l.swansbra@gmail.com>
2022-09-24 20:03:24 +1000

    Added tailwind CSS to the project for use

--- coffee_robots/src/App.js
+++ coffee_robots/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <HelloUser name="Rhys Swansbra"/>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
       </header>
     </div>
   );
