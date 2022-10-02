bdef713464f2fa16acb953cb75c068cd859d11b2
rswansbra <r.l.swansbra@gmail.com>
2022-10-02 19:11:09 +1100

    gET IMAGES TO WORK

--- coffee_robots/src/MyComponents/coffeeBox.js
+++ coffee_robots/src/MyComponents/coffeeBox.js
@@ -8,6 +8,7 @@ class CoffeeBox extends React.Component {
                 <img
                 className="object-cover w-full lg:w-40 lg:h-40"
                 src="coffee-cardEG.png"
+                alt=""
                 />
                 <div className="p-4">
                     <button className="px-4 py-2 font-comfortaaBold text-sm text-coffeeCustom bg-coffeeDark rounded shadow">
